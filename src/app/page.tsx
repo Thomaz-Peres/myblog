ac76ea3cc1ef4a0f5ea7bd8b5a669ad029db9165
Thomaz <thomaz.peres21@hotmail.com>
2024-02-23 23:04:34 -0300

    *

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@
 
 import { Separator } from "@/components/ui/separator";
 import { ArchiveIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
@@ -22,6 +30,18 @@ export default function Home() {
       <Separator className="my-5" />
       <div>
         <h3 className="text-lg font-bold mb-2">Projects</h3>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
 
         <p className="italic underline"><a href="/projects"> Ver mais {'->'} </a></p>
       </div>
