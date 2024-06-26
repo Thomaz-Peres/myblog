496e2a68b571162ff2e44702fdc0daea845701a5
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-06-26 19:58:59 -0300

    some config updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 const withMDX = require('@next/mdx')()
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  pageExtensions: ['js', 'jsx', 'mdx', 'md', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      }
+    ],
+  },
 }
- 
+
 module.exports = withMDX(nextConfig)
