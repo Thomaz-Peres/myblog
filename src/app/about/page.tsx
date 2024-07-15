483a4a8f37226cf6fd49670c0139710bebf44ec7
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-07-15 19:12:41 -0300

    Fixing build and about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,12 +9,12 @@ export default function Page() {
       with a strong focus on the back-end. I have experience with <strong>.NET Framework/Core, SQL, Docker, Kubernetes, Redis, Azure, Unit/Performance/Automation Tests
       and Scrum/Kanban</strong>. Also, used TypeScript, JavaScript, HTML, CSS, React, Angular, Tailwind and MixPanel.</p> */}
       <p className="text-xl font-semibold mt-5">Hello!</p>
-      <p className="mt-3">My name is Thomaz Peres, I am a Software Engineer professionally since october/2020.</p>
-      <br/>
-        <p>Within all those years, I've developed and architected different high performance and scalable applications.
-        I’m a <a className="underline decoration-red-500">full-stack developer</a> with focus on <a className="underline decoration-red-500">the back-end</a>. I have experience with <strong>
-          .NET Framework/Core, SQL, CosmosDb, Events Architecture, Docker, Kubernetes, Redis, Azure, Unit/Performance/Automation Tests and Scrum/Kanban</strong>.
-        Also, used TypeScript, JavaScript, HTML, CSS, React, Angular, Tailwind and MixPanel.</p>
+      <p className="mt-3">My name is Thomaz Peres, I&apos;m a Software Engineer professionally since october/2020. I specialize in full-stack development with a focus on the back-end.</p>
+      <br />
+      <p>Throughout my career all those years, I&apos;ve developed and architected various high performance and scalable applications. I&apos;ve experience with <strong>
+        .NET Framework/Core, SQL, CosmosDb, Events Architecture, Docker, Kubernetes, Redis, Azure, Unit/Performance/Automation Tests.
+        I&apos;m also familiar with Scrum and Kanban methodologies</strong>.
+        On the front-end, I&apos;ve worked with TypeScript, JavaScript, HTML, CSS, React, Angular, Tailwind and MixPanel.</p>
     </div>
   );
 }
