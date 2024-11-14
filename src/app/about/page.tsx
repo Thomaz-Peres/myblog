f5cbd22103eccbdb4e31bd8076db5cf7a540532e
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-11-13 22:55:33 -0300

    Now really done, nice (While more I open, more I want to change all the site and colors)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,6 @@
 export default function Page() {
   return (
     <div className="mt-10 ml-105 xl:w-7/12 lg:w-4/5 w-11/12 mb-10">
-      {/* <BreadcrumbComponent /> */}
-      {/* <div className="mt-5 w-52 border-t-[3px] hr"/> */}
-      {/* // 
-      <p className="mt-3">My name is Thomaz Peres, I am a Software Engineer professionally since <strong>october/2020</strong>.
-      Within all those years, I have developed and architected different high performance and scalable applications. I’m a <strong>full-stack developer</strong>
-      with a strong focus on the back-end. I have experience with <strong>.NET Framework/Core, SQL, Docker, Kubernetes, Redis, Azure, Unit/Performance/Automation Tests
-      and Scrum/Kanban</strong>. Also, used TypeScript, JavaScript, HTML, CSS, React, Angular, Tailwind and MixPanel.</p> */}
       <p className="text-xl font-semibold mt-5">Hello!</p>
       <p className="mt-3">My name is Thomaz Peres, I&apos;m a Software Engineer professionally since october/2020. I specialize in full-stack development with a focus on the back-end.</p>
       <br />
