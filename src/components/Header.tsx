8b08d833beb7d5cc3757f660ee7aea33ccbfa24a
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-05-03 18:33:00 -0300

    *

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
 
     return (
         <header className="px-0 py-6 md:justify-smart gap-x-3 flex flex-1 items-center justify-between">
-            <Link href="/" className="text-base font-bold mb-2">Thomaz`&apos;`s blog</Link>
+            <Link href="/" className="text-base font-bold mb-2">Thomaz&apos;s blog</Link>
             <div className="flex justify-start items-center space-x-4">
                 <Link href="https://www.github.com/Thomaz-Peres"><FaGithub className="size-5" /> </Link>
                 <p className="text-xs">•</p>
